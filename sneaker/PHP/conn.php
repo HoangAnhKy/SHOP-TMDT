57b46bc5bf6653bac5f1e7a725ec5090eaa28984
ky hoang <hoanganhkyltt@gmail.com>
2022-03-10 00:08:54 +0700

    first commit

--- sneaker/PHP/conn.php
+++ sneaker/PHP/conn.php
@@ -1,5 +1,6 @@
 <?php
 
 // $connect = new PDO("mysql:host=sql105.epizy.com; dbname=epiz_30633101_w875","epiz_30633101","LTAnWyHd7rZv5"); host
+// $connect = new PDO("mysql:host=localhost; dbname=tmdt","admin","aA@123456");
 $connect = new PDO("mysql:host=localhost; dbname=product_tmdt","root","");
 ?>
